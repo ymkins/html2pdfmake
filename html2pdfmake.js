8ff6fa3acd70cbd198b9abebe77bdf11c0fc0696
Pavlo Sumkin <psumkin@mirantis.com>
2015-09-17 21:50:28 +0300

    [js] Format JS

--- html2pdfmake.js
+++ html2pdfmake.js
@@ -12,174 +12,192 @@ var html2pdfmake = function (html) {
     function ParseContainer(cnt, e, p, styles) {
         var elements = [];
         var children = e.childNodes;
-        if (children.length != 0) {
-            for (var i = 0; i < children.length; i++) p = ParseElement(elements, children[i], p, styles);
+        if (children.length !== 0) {
+            for (var i = 0; i < children.length; i++) {
+                p = ParseElement(elements, children[i], p, styles);
+            }
         }
-        if (elements.length != 0) {
-            for (var i = 0; i < elements.length; i++) cnt.push(elements[i]);
+        if (elements.length !== 0) {
+            for (var i = 0; i < elements.length; i++) {
+                cnt.push(elements[i]);
+            }
         }
         return p;
     }
 
     function ComputeStyle(o, styles) {
         for (var i = 0; i < styles.length; i++) {
-            var st = styles[i].trim().toLowerCase().split(":");
-            if (st.length == 2) {
+            var st = styles[i].trim().toLowerCase().split(':');
+            if (st.length === 2) {
                 switch (st[0]) {
-                    case "font-size":{
+                    case 'font-size':
                         o.fontSize = parseInt(st[1]);
                         break;
-                    }
-                    case "text-align": {
+                    case 'text-align':
                         switch (st[1]) {
-                            case "right": o.alignment = 'right'; break;
-                            case "center": o.alignment = 'center'; break;
+                            case 'right':
+                                o.alignment = 'right';
+                                break;
+                            case 'center':
+                                o.alignment = 'center';
+                                break;
                         }
                         break;
-                    }
-                    case "font-weight": {
+                    case 'font-weight':
                         switch (st[1]) {
-                            case "bold": o.bold = true; break;
+                            case 'bold':
+                                o.bold = true;
+                                break;
                         }
                         break;
-                    }
-                    case "text-decoration": {
+                    case 'text-decoration':
                         switch (st[1]) {
-                            case "underline": o.decoration = "underline"; break;
+                            case 'underline':
+                                o.decoration = 'underline';
+                                break;
                         }
                         break;
-                    }
-                    case "font-style": {
+                    case 'font-style':
                         switch (st[1]) {
-                            case "italic": o.italics = true; break;
+                            case 'italic':
+                                o.italics = true;
+                                break;
                         }
                         break;
-                    }
                 }
             }
         }
     }
 
     function ParseElement(cnt, e, p, styles) {
-        if (!styles) styles = [];
+        if (!styles)
+            styles = [];
         if (e.getAttribute) {
-            var nodeStyle = e.getAttribute("style");
+            var nodeStyle = e.getAttribute('style');
             if (nodeStyle) {
-                var ns = nodeStyle.split(";");
-                for (var k = 0; k < ns.length; k++) styles.push(ns[k]);
+                var ns = nodeStyle.split(';');
+                for (var k = 0; k < ns.length; k++) {
+                    styles.push(ns[k]);
+                }
             }
         }
 
         switch (e.nodeName.toLowerCase()) {
-            case "#text": {
-                var t = { text: e.textContent.replace(/\n/g, "") };
-                if (styles) ComputeStyle(t, styles);
+            case '#text':
+                var t = {text: e.textContent.replace(/\n/g, '')};
+                if (styles) {
+                    ComputeStyle(t, styles);
+                }
                 p.text.push(t);
                 break;
-            }
-            case "b":case "strong": {
-                //styles.push("font-weight:bold");
-                ParseContainer(cnt, e, p, styles.concat(["font-weight:bold"]));
+            case 'b':
+            case 'strong':
+                //styles.push('font-weight:bold');
+                ParseContainer(cnt, e, p, styles.concat(['font-weight:bold']));
                 break;
-            }
-            case "u": {
-                //styles.push("text-decoration:underline");
-                ParseContainer(cnt, e, p, styles.concat(["text-decoration:underline"]));
+            case 'u':
+                //styles.push('text-decoration:underline');
+                ParseContainer(cnt, e, p, styles.concat(['text-decoration:underline']));
                 break;
-            }
-            case "i": {
-                //styles.push("font-style:italic");
-                ParseContainer(cnt, e, p, styles.concat(["font-style:italic"]));
+            case 'i':
+                //styles.push('font-style:italic');
+                ParseContainer(cnt, e, p, styles.concat(['font-style:italic']));
                 //styles.pop();
                 break;
                 //cnt.push({ text: e.innerText, bold: false });
-            }
-            case "span": {
+            case 'span':
                 ParseContainer(cnt, e, p, styles);
                 break;
-            }
-            case "br": {
+            case 'br':
                 p = CreateParagraph();
                 cnt.push(p);
                 break;
-            }
-            case "table":
-                {
-                    var t = {
-                        table: {
-                            widths: [],
-                            body: []
-                        }
+            case 'table':
+                var t = {
+                    table: {
+                        widths: [],
+                        body: []
                     }
-                    var border = e.getAttribute("border");
-                    var isBorder = false;
-                    if (border) if (parseInt(border) == 1) isBorder = true;
-                    if (!isBorder) t.layout = 'noBorders';
-                    ParseContainer(t.table.body, e, p, styles);
+                };
+                var border = e.getAttribute('border');
+                var isBorder = false;
+                if (border)
+                    if (parseInt(border) === 1)
+                        isBorder = true;
+                if (!isBorder)
+                    t.layout = 'noBorders';
+                ParseContainer(t.table.body, e, p, styles);
 
-                    var widths = e.getAttribute("widths");
-                    if (!widths) {
-                        if (t.table.body.length != 0) {
-                            if (t.table.body[0].length != 0) for (var k = 0; k < t.table.body[0].length; k++) t.table.widths.push("*");
+                var widths = e.getAttribute('widths');
+                if (!widths) {
+                    if (t.table.body.length !== 0) {
+                        if (t.table.body[0].length !== 0) {
+                            for (var k = 0; k < t.table.body[0].length; k++) {
+                                t.table.widths.push('*');
+                            }
                         }
-                    } else {
-                        var w = widths.split(",");
-                        for (var k = 0; k < w.length; k++) t.table.widths.push(w[k]);
                     }
-                    cnt.push(t);
-                    break;
+                } else {
+                    var w = widths.split(',');
+                    for (var k = 0; k < w.length; k++) {
+                        t.table.widths.push(w[k]);
+                    }
                 }
-            case "tbody": {
+                cnt.push(t);
+                break;
+            case 'tbody':
                 ParseContainer(cnt, e, p, styles);
                 //p = CreateParagraph();
                 break;
-            }
-            case "tr": {
+            case 'tr':
                 var row = [];
                 ParseContainer(row, e, p, styles);
                 cnt.push(row);
                 break;
-            }
-            case "td": {
+            case 'td':
                 p = CreateParagraph();
-                var st = {stack: []}
+                var st = {stack: []};
                 st.stack.push(p);
 
-                var rspan = e.getAttribute("rowspan");
-                if (rspan) st.rowSpan = parseInt(rspan);
-                var cspan = e.getAttribute("colspan");
-                if (cspan) st.colSpan = parseInt(cspan);
+                var rspan = e.getAttribute('rowspan');
+                if (rspan) {
+                    st.rowSpan = parseInt(rspan);
+                }
+                var cspan = e.getAttribute('colspan');
+                if (cspan) {
+                    st.colSpan = parseInt(cspan);
+                }
 
                 ParseContainer(st.stack, e, p, styles);
                 cnt.push(st);
                 break;
-            }
-            case "div":case "p": {
+            case 'div':
+            case 'p':
                 p = CreateParagraph();
-                var st = {stack: []}
+                var st = {stack: []};
                 st.stack.push(p);
                 ComputeStyle(st, styles);
                 ParseContainer(st.stack, e, p);
 
                 cnt.push(st);
                 break;
-            }
-            default: {
-                console.log("Parsing for node " + e.nodeName + " not found");
+            default:
+                console.log('Parsing for node ' + e.nodeName + ' not found');
                 break;
-            }
         }
         return p;
     }
 
     function ParseHtml(cnt, htmlText) {
-        var html = $(htmlText.replace(/\t/g, "").replace(/\n/g, ""));
+        var html = $(htmlText.replace(/\t/g, '').replace(/\n/g, ''));
         var p = CreateParagraph();
-        for (var i = 0; i < html.length; i++) ParseElement(cnt, html.get(i), p);
+        for (var i = 0; i < html.length; i++) {
+            ParseElement(cnt, html.get(i), p);
+        }
     }
 
     function CreateParagraph() {
-        var p = {text:[]};
+        var p = {text: []};
         return p;
     }
 
